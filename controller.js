fc2a97874b1e37a074f9356a1d0f865adef31d07
Adam Bailey <33302008+adamjbailey@users.noreply.github.com>
2018-01-12 21:14:56 -0500

    Styling

--- controller.js
+++ controller.js
@@ -2,9 +2,15 @@ var lockedDown = false;
 function toggleLock(){
     if(lockedDown){
         document.getElementById('foreground').classList.remove('locked');
+        document.getElementById('lockdown').classList.remove('lockdown');
+        document.getElementById('menuBar').classList.remove('slide-left');
+        document.getElementById('greeting').classList.remove('locked');
         lockedDown = false;
     } else {
         document.getElementById('foreground').classList.add('locked');
+        document.getElementById('lockdown').classList.add('lockdown');
+        document.getElementById('menuBar').classList.add('slide-left');
+        document.getElementById('greeting').classList.add('locked');
         lockedDown = true;
     }
 }
